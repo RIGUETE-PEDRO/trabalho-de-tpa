1d69aea25508c8626cefc063d4bb16553f070529
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2025-06-23 11:04:07 -0300

    496 a 574 falta comentar o cod

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 
 #include "prototipo.h"
 
+//limpar o terminal
 void limpar()
 {
 #ifdef _WIN32
@@ -15,15 +16,23 @@ int main(int argc, char const *argv[])
     int id = 0;
     int opcao = 0;
 
+    //cria estrututra ao iniciar a main
     Lista* lista =criar_estrutura();
     
+    //limpa o terminal
     limpar();
     do
     {
+        //exibe menu
         menu();
         
+        //pega a opcao desejada
         scanf("%i",&opcao);
+
+        //analisa qual sera a funçao que sera chamada para atender a necessidade da pessoa
         processar_opcao(&opcao,&id,lista);
+
+        // ate que seja diferente de zero
     } while (opcao!=0);
     
     return 0;
